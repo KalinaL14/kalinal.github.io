7454b926a8aea0f04e62095c12496422b9dae6c6
l.kalina <l.kalina@vedomosti.ru>
2023-02-04 23:39:41 +0400

    feature(index): add new box #1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('call').onclick = function() {
+    document.getElementById('modal-open').hidden = false;
+}
+
+document.getElementById('closed').onclick = function() {
+    document.getElementById('modal-open').hidden = true;
+}
